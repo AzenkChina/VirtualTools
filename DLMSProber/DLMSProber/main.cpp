8bda9009984ea7337f0b4efb7b6673b4e8ef6d37
AzenkChina <azenk1991@163.com>
2020-06-02 09:56:54 +0800

    Define the window style.

--- DLMSProber/DLMSProber/main.cpp
+++ DLMSProber/DLMSProber/main.cpp
@@ -4,8 +4,10 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+#if defined(Q_OS_WIN32)
+    QApplication::setStyle("fusion");
+#endif
     Prober w;
     w.show();
-
     return a.exec();
 }
--- VirtualDisplay/VirtualDisplay/main.cpp
+++ VirtualDisplay/VirtualDisplay/main.cpp
@@ -4,6 +4,9 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+#if defined(Q_OS_WIN32)
+    QApplication::setStyle("fusion");
+#endif
     MainWindow w;
     w.show();
     return a.exec();
